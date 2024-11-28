ecb9d936785b4dc73d366989df9ce5ce57351ec4
Yannick Jud <118299935+Bansker@users.noreply.github.com>
2024-11-28 09:43:34 +0100

    Delete src/_BACKUP directory

--- src/_BACKUP/globals.py
+++
@@ -1,6 +0,0 @@
-# --- GLOBAL CONSTANTS --- #
-PATH_DIR_SD_DATA_FORISCONTROL = ':/foriscontrol' # Drive letter will be assigned at runtime in manage_files.py
-PATH_DIR_MAIN = './files_foriscontrol'
-PATH_DIR_CSV = PATH_DIR_MAIN + '/csv'
-PATH_DIR_CONFIGS = PATH_DIR_MAIN + '/configs'
-
